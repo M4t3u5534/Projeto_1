c47635dbf59c30788939b57784bed6b8f4dcc2db
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-03-07 12:17:39 +0000

    EXIT

--- main.java
+++ main.java
@@ -1,3 +1,15 @@
 // Carolina Lee - 10440304
 // Mateus Ribeiro Cerqueira - 10443901
 //Pedro Carvalho - 10418861
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String verificar = "";
+        while(verificar != "EXIT"){
+            verificar = scanner.nextLine();
+        }
+        scanner.close();
+    }
+}
